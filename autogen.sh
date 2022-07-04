8326ce9aa4053ef9489c7d242e8b49db7d0deaef
HaNeul-Kim <hskimsky@gmail.com>
2022-07-04 15:38:42 +0900

    option 변경

--- autogen.sh
+++ autogen.sh
@@ -19,7 +19,7 @@ fi
 # directory is set up as an SVN external.
 if test ! -e gtest; then
   echo "Google Test not present.  Fetching gtest-1.5.0 from the web..."
-  curl -L https://github.com/google/googletest/archive/release-1.5.0.tar.gz | tar jx
+  curl -L https://github.com/google/googletest/archive/release-1.5.0.tar.gz | tar zx
   mv googletest-release-1.5.0 gtest
 fi
 
